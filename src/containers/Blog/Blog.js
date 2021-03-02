dbfda4ee5f3de390c0aacbce47d9d3c4b7097556
jordles113 <jordan.t.romero@gmail.com>
2021-03-02 15:57:05 -0600

    dynamically rendering post titles

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,18 +7,24 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: [],
+  };
+
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts');
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      this.setState({ posts: response.data });
+      console.log(this.state.posts);
+    });
   }
 
   render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post key={post.id} title={post.title} />;
+    });
     return (
       <div>
-        <section className='Posts'>
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className='Posts'>{posts}</section>
         <section>
           <FullPost />
         </section>
