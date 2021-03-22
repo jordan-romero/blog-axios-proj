4d9e3a6912b1e8ab1d28be7cdadcc159382b7cbf
jordles113 <jordan.t.romero@gmail.com>
2021-03-22 18:26:15 -0500

    styles the active link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,34 +1,52 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</Link></li>
-                        </ul>
-                    </nav>
-                </header>
-                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+  render() {
+    return (
+      <div className='Blog'>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to='/'
+                  exact
+                  activeClassName='active'
+                  activeStyle={{
+                    color: '#fa923f',
+                    textDecoration: 'underline',
+                  }}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true',
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-            </div>
-        );
-    }
+        <Route path='/' exact component={Posts} />
+        <Route path='/new-post' component={NewPost} />
+      </div>
+    );
+  }
 }
 
 export default Blog;
