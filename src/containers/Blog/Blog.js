07cc46e610a697277e0c096ca51fce2feb68a6fe
jordles113 <jordan.t.romero@gmail.com>
2021-03-22 20:48:31 -0500

    messing wit hnested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,53 +4,40 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-  render() {
-    return (
-      <div className='Blog'>
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <NavLink
-                  to='/'
-                  exact
-                  activeClassName='active'
-                  activeStyle={{
-                    color: '#fa923f',
-                    textDecoration: 'underline',
-                  }}
-                >
-                  Posts
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to={{
-                    pathname: '/new-post',
-                    hash: '#submit',
-                    search: '?quick-submit=true',
-                  }}
-                >
-                  New Post
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-        <Switch>
-          <Route path='/' exact component={Posts} />
-          <Route path='/new-post' component={NewPost} />
-          <Route path='/:id' exact component={FullPost} />
-        </Switch>
-      </div>
-    );
-  }
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default Blog;
